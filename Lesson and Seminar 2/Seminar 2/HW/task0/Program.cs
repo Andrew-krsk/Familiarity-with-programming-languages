dc53c3169e9b1347ce20afdd25fc6274991cafc5
Andrew <>
2022-09-04 10:43:02 +0700

    до

---
+++ Lesson and Seminar 2/Seminar 2/HW/task0/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine() ?? "0";
+    bool isNumber = int.TryParse(strValue, out int value);
+    if (isNumber)
+    {
+        return value;
+    }
+    throw new Exception("Данное значение невозможно преобразовать в число");
+}
+int value = Prompt("Введите трехзначное число > ");
+
+int SecondNumber (int second)
+{
+    int result = second % 100;
+    result = result / 10;
+    return result;
+}
+
+if (value < 1000 && value > 99)
+{
+    int secondNumber = SecondNumber(value);
+    System.Console.WriteLine($"вторая цифра трезначного числа -> {secondNumber}");
+}
+else
+{
+    System.Console.WriteLine("Ошибка. Введите трехзначное положительное число");
+}
