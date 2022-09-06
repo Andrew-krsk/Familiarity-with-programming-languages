d86aa4c067ddde40770de090b63d61ca2c224d9b
Andrew <>
2022-09-06 14:27:01 +0700

    После всего дз к Семинару 3 06.09.22

--- Lesson and Seminar 3/Seminar 3/HW/task0/Program.cs
+++ Lesson and Seminar 3/Seminar 3/HW/task0/Program.cs
@@ -2,3 +2,18 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int value = Prompt("Введите число:");
+
+if (value/10000 == value %10 && value/1000 %10 == value %100 /10)
+{
+    System.Console.WriteLine($" Число {value} является полиндромом");
+}
+else
+System.Console.WriteLine($" Число {value} не является полиндромом");
