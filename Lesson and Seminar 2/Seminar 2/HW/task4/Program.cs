dfc6174a18936b623cfaf991f97fecabea7bea3d
Andrew <>
2022-09-06 12:01:50 +0700

    После таск 4* дз к Семинару 2 06.09.22

---
+++ Lesson and Seminar 2/Seminar 2/HW/task4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая генерирует несколько случайных чисел, и в цикле проверяет, 
+// кратны ли эти числа предварительно введенному числу, при кратности - цикл прерывается.
+// Введенное число 2
+// 13 -> нет
+// 15 -> нет
+// 12 -> да
+
+
+int value1 = new Random().Next(10, 100);
+int value2 = new Random().Next(10, 100);
+int value3 = new Random().Next(10, 100);
+
+while (value1 %2 == 0 || value2 %2 == 0 || value3 %2 ==0)
+{
+    if (value1 %2 == 0)
+    {
+        System.Console.WriteLine($"{value1} -> Да");
+        break;
+    }
+    else
+    System.Console.WriteLine($"{value1} -> Нет");
+   
+    if (value2 %2 == 0)
+    {
+        System.Console.WriteLine($"{value2} -> Да");
+        break;
+    }
+    else
+    System.Console.WriteLine($"{value2} -> Нет");
+
+    if (value3 %2 == 0)
+    {
+        System.Console.WriteLine($"{value3} -> Да");
+        break;
+    }
+    else
+    System.Console.WriteLine($"{value3} -> Нет");
+}
