8a2456ad6e2c45d2b7871fc29e174f12852a8a65
Andrew <>
2022-09-12 15:27:40 +0700

    После task2 hw seminar 5 12.09.22

---
+++ Lesson and Seminar 5/Seminar 5/HW/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenerateArray(int length, int minRandom, int maxRandom)
+{
+    Random rnd = new Random();
+    int[] answer = new int[length];
+    for (int i = 0; i < answer.Length; i++)
+    {
+        answer[i] = rnd.Next(minRandom, maxRandom + 1);
+    }
+    return answer;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + ", ");
+    }
+    System.Console.WriteLine();
+}
+
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        count = count + array[i];
+    }
+    return count;
+}
+
+int[] array = GenerateArray(5, 0, 9);
+PrintArray(array);
+System.Console.WriteLine(Count(array));
