fc772c34a73efabf800dc67a9396a81b2ac76f6f
Andrew <>
2022-09-12 19:08:02 +0700

    После hw seminar 4 + task4* 12.09.22

--- Lesson and Seminar 4/Seminar 4/HW/task4/Program.cs
+++ Lesson and Seminar 4/Seminar 4/HW/task4/Program.cs
@@ -6,3 +6,47 @@
 // 1 2 1 2 1 1 1 1 -> 1
 // 1 2 3 4 5 6 7 8 -> 7
 // 1 2 3 4 5 6 8 7 -> 7
+
+int[] Create(int length)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+    return array;
+}
+void DisplayArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write(" -> ");
+}
+
+int FindMax(int[] array)
+{
+    int max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+int FindSecondMax(int[] array, int max)
+{
+    int max2 = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max2 < array[i] && array[i] < max) max2 = array[i];
+    }
+    return max2;
+}
+int[] array = Create(8);
+DisplayArray(array);
+int max = FindMax(array);
+int max2 = FindSecondMax(array, max);
+System.Console.WriteLine($"Второй максимум = {max2}");
